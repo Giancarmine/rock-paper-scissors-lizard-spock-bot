d0a8e88d333a3111aa00034a8c743a52a99f2482
El_Giancar <giancarmine.cuofano@gmail.com>
2022-12-16 20:46:18 +0100

    Add /healthz endpoint to render.com

--- src/main/kotlin/com/el_giancar/plugins/Routing.kt
+++ src/main/kotlin/com/el_giancar/plugins/Routing.kt
@@ -1,16 +1,19 @@
 package com.el_giancar.plugins
 
-import io.ktor.server.routing.*
 import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
-import io.ktor.server.request.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
 
     routing {
         get("/") {
             call.respondText("Hello World!")
+        }
+
+        get("/healthz") {
+            call.respond(HttpStatusCode.OK, "OK")
         }
     }
 }
